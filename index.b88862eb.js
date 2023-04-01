8f49d589095f7889519429a31462afbea5b7869c
OleksandrPM <114025110+OleksandrPM@users.noreply.github.com>
2023-04-01 23:34:34 +0000

    Deploying to gh-pages from @ OleksandrPM/goit-js-hw-11@e669f37a5208c56cbaf9282968294c7f73284ed0 🚀

--- index.b88862eb.js
+++
@@ -1,2 +0,0 @@
-const t={baseUrl:"https://pixabay.com/api/",key:"34821763-cd0390e9b5fa3f24bbb43d369",q:function(t){return t},image_type:"photo",orientation:"horizontal",safesearch:!0};(function(e){const o=function(e){const{baseUrl:o,key:n,q:a,image_type:r,orientation:s,safesearch:i}=t;return`${o}?key=${n}&q=${a(e)}&image_type=${r}&orientation=${s}&safesearch=${i}`}(e);return fetch(o).then((t=>{if(!t.ok)throw new Error(t.status);return t.json()}))})("flowers").then((t=>{console.log(t.totalHits,t.total)}));
-//# sourceMappingURL=index.b88862eb.js.map
